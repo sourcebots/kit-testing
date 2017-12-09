76266534a0447ff3fabd4f677018b03cca301e1c
Jake Howard <git@theorangeone.net>
2017-12-09 11:40:15 +0000

    Add buzzing tests

--- test_power.py
+++ test_power.py
@@ -8,3 +8,14 @@ def test_power_board(power_board):
         sleep(1)
         print("Turning off outputs")
         power_board.power_off()
+
+    for note, frequency in power_board.BUZZ_NOTES.items():
+        print("Playing {} on buzzer".format(note))
+        power_board.buzz(0.5, note=note)
+        sleep(0.5)
+        print("Playing {} on buzzer".format(note.upper()))
+        power_board.buzz(0.5, note=note.upper())
+        sleep(0.5)
+        print("Playing {} on buzzer as frequency {}".format(note, frequency))
+        power_board.buzz(0.5, frequency=frequency)
+        sleep(0.5)
