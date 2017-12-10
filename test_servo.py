90b3a2e50635f51a9bbb5490278238cad32fd961
Jake Howard <git@theorangeone.net>
2017-12-10 11:41:21 +0000

    Learn how brackets work

--- test_servo.py
+++ test_servo.py
@@ -22,23 +22,23 @@ def test_analogue(servo_assembly):
 
 def test_servos(servo_assembly):
     for servo_pin in range(0, 16):
-        print("Setting servo {} to {}".format(servo_pin, SERVO_UPPER_BOUND)))
+        print("Setting servo {} to {}".format(servo_pin, SERVO_UPPER_BOUND))
         servo_assembly.servos[servo_pin].position = SERVO_UPPER_BOUND
         sleep(1)
-        print("Setting servo {} to {}".format(servo_pin, SERVO_LOWER_BOUND)))
+        print("Setting servo {} to {}".format(servo_pin, SERVO_LOWER_BOUND))
         servo_assembly.servos[servo_pin].position = SERVO_LOWER_BOUND
         sleep(1)
 
     for i in range(SERVO_LOWER_BOUND, SERVO_UPPER_BOUND, 10):
         for servo_pin in range(0, 16):
-            print("Setting servo {} to {}".format(servo_pin, i)))
+            print("Setting servo {} to {}".format(servo_pin, i))
             servo_assembly.servos[servo_pin].position = i
             assert servo_assembly.servos[servo_pin].position == i
             sleep(0.2)
 
     for i in range(SERVO_UPPER_BOUND, SERVO_LOWER_BOUND, -10):
         for servo_pin in range(0, 16):
-            print("Setting servo {} to {}".format(servo_pin, i)))
+            print("Setting servo {} to {}".format(servo_pin, i))
             servo_assembly.servos[servo_pin].position = i
             assert servo_assembly.servos[servo_pin].position == i
             sleep(0.2)
