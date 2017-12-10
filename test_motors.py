0d6eaf3e0c2e6df8e9d83a9c3a6588eefe588ec7
Jake Howard <git@theorangeone.net>
2017-12-10 12:02:36 +0000

    Set motors to half before full power

--- test_motors.py
+++ test_motors.py
@@ -3,14 +3,21 @@ from time import sleep
 
 
 def test_motor_board(motor_board):
+    motor_board.m0 = 0.5
+    motor_board.m1 = 0.5
+    sleep(1)
+    print("Setting motors to full power")
     motor_board.m0 = 1
     motor_board.m1 = 1
-    print("Setting motors to full power")
     sleep(3)
     print("Coasting motors")
     motor_board.m0 = COAST
     motor_board.m1 = COAST
     sleep(2)
+
+    motor_board.m0 = -0.5
+    motor_board.m1 = -0.5
+    sleep(1)
     print("Setting motors to full power (reverse)")
     motor_board.m0 = -1
     motor_board.m1 = -1
@@ -41,6 +48,9 @@ def test_motor_board(motor_board):
     motor_board.m1 = COAST
     sleep(1)
 
+    motor_board.m0 = 0.5
+    motor_board.m1 = 0.5
+    sleep(1)
     for i in range(-100, 101, 2):
         print("Setting motor power to ", i / 100)
         motor_board.m0 = i / 100
